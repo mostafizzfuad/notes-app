8b4944c19932f6fb238a39c0a7e0cb1445b1b538
Mostafizur <mostafizzfuad@gmail.com>
2025-12-30 22:41:54 +0600

    release: notes app v1.0

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note";
+
 const NoteList = ({ notes, deleteNote }) => {
 	return (
 		<div className="space-y-4">
@@ -8,27 +10,8 @@ const NoteList = ({ notes, deleteNote }) => {
 
 			{/* নোট থাকলে ম্যাপ করে দেখাও */}
 			{notes.map((note) => (
-				<div
-					key={note.id}
-					className="p-4 bg-white rounded-lg shadow-md border-l-4 border-purple-500"
-				>
-					<h3 className="text-lg font-bold">{note.title}</h3>
-					<p className="text-sm text-gray-600">
-						<strong>Category:</strong> {note.category}
-					</p>
-					<p className="text-sm text-gray-600">
-						<strong>Priority:</strong> {note.priority}
-					</p>
-					<p className="mt-2 text-gray-800">{note.description}</p>
-
-					{/* Delete Button */}
-					<button
-						className="mt-3 text-red-500 hover:text-red-700 cursor-pointer transition font-semibold"
-						onClick={() => deleteNote(note.id)}
-					>
-						🗑 Delete
-					</button>
-				</div>
+				// এখানে আমরা Note কম্পোনেন্ট রেন্ডার করছি এবং প্রপস পাস করছি
+				<Note key={note.id} note={note} deleteNote={deleteNote} />
 			))}
 		</div>
 	);
